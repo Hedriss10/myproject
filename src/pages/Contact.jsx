054c7ec2f98908b834584718b10b072065c4a225
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-30 11:55:54 -0300

    changes: add map in contactlist

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,9 +2,15 @@ import React from "react";
 
 
 const Contact = () => {
+    const contactList = ["Alex", "Jhon", "Maria"];
     return (
         <div>
             <h1>Contact Page</h1>
+            <ul>
+                {contactList.map((contact, index) => (
+                    <li key={index}>{contact}</li>
+                ))}
+            </ul>
             <p>This is the contact page.</p>
         </div>
     );
