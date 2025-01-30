1039aabc5f9d2e6dea65a475e63851dfaa2e5ee4
Hedris Pereira <hedris.pereira@eemovel.com>
2025-01-30 12:21:34 -0300

    changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,10 +2,12 @@ import React from "react";
 
 
 const Contact = () => {
+
     const contactList = ["Alex", "Jhon", "Maria"];
     return (
         <div>
             <h1>Contact Page</h1>
+            <button onClick={() => alert('clique')}>Click</button>
             <ul>
                 {contactList.map((contact, index) => (
                     <li key={index}>{contact}</li>
