a8f844e476b6f30103ad7da612b48165f2cab051
Hedris Pereira <hedris.pereira@eemovel.com>
2025-02-01 17:42:10 -0300

    fix: ajudst project with students

---
+++ react-students-js/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p>Todo-list with react</p>
+  )
+}
+
+export default App
