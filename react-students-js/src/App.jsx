c4b9a520ce91904ac9f28f30e6c09823e08ad18b
Hedris Pereira <hedris.pereira@eemovel.com>
2025-02-01 19:10:02 -0300

    project with talwind css

--- react-students-js/src/App.jsx
+++ react-students-js/src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 import "./App.css";
 
 function App() {
-  return <p>Todo-list with react</p>;
+  return (
+    <Router>
+      <Header />
+      <div className="container mx-auto p-4">
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact/:name" element={<ContactDetail />} />
+          <Route path="/products" element={<Product />} />
+        </Routes> */}
+      </div>
+    </Router>
+  );
 }
 
 export default App;
